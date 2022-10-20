2985257614a57f04f67d52fc2efd1128580d070e
David Schwartz <49711841+dschwartzi@users.noreply.github.com>
2022-10-20 23:53:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,9 @@ setup(
     url='https://github.com/dschwartz/saas-co-cur',
     keywords='cost optimization cur',
     install_requires=[
-          'os',
           'boto3',
           'botocore',
           'pandas',
-          'awswrangler',
-          'functools'
+          'awswrangler'
       ],
 )
