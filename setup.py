1b70d3f79aa8193d5f26d8da140e5047f8449663
David Schwartz <49711841+dschwartzi@users.noreply.github.com>
2022-10-21 15:27:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='saas_co',
-    version='0.11',
+    version='0.12',
     license='MIT',
     author="David Schwartz",
     author_email='david.schwartz@devfactory.com',
